168daf302e02cf87c8cfd19a4042982d9250e129
Ruben <rubenpenott@hotmail.com>
2024-07-06 17:17:06 -0400

    signup added

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')), # for authentication
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
     ]
