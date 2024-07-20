8d771a7cdfec59a9e77ddb73f8a3d488ba8e562a
Ruben <rubenpenott@hotmail.com>
2024-07-20 17:59:37 -0400

    model books added

---
+++ books/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Book
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price')
+    list_filter = ('author',)
+    search_fields = ('title', 'author', 'description')
+
+admin.site.register(Book, BookAdmin)
