cd403d78c022b9a4f1bc038589943d620da58ce9
Ruben <rubenpenott@hotmail.com>
2024-09-14 17:52:16 -0400

    funcionalidad para la barra de busqueda. parte 2

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 # Create your views here.
 
@@ -18,3 +19,8 @@ class SearchResultsView(ListView):
     model = Book
     template_name = 'books/search.html'
     
+    def get_queryset(self):
+        query = self.request.GET.get('q')       #para buscar por título
+        return Book.objects.filter(                                
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )           
