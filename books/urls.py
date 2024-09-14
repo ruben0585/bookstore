2eea4c1488ed9547fe24f80598342f5c399cbfc0
Ruben <rubenpenott@hotmail.com>
2024-09-14 16:43:40 -0400

    funcionalidad para la barra de busqueda. parte 1

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'), #la dirección será '' ya que quiero que la lsta de libros aparezca en el home
     path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsView.as_view(), name='search_results')
 ]
--- books/views.py
+++ books/views.py
@@ -13,3 +13,8 @@ class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     login_url = '/accounts/login/'
+
+class SearchResultsView(ListView):
+    model = Book
+    template_name = 'books/search.html'
+    
